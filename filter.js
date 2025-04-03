24ec8f92da5bae6136ebc43dd7b7e5ebd5390d40
Rafael Alcon <rafa_prog@hotmail.com>
2025-04-03 13:18:38 -0400

    filter excercise

---
+++ filter.js
@@ -0,0 +1,89 @@
+const numbers = [1, 60, 112, 123, 100, 99, 73, 45];
+const places = [
+    {
+      title: "Awesome Suite 20' away from la Rambla",
+      price: 200,
+      type: 'Private Room',
+      pool: true,
+      garage: false
+    },
+    {
+      title: 'Private apartment',
+      price: 190,
+      type: 'Entire Place',
+      pool: true,
+      garage: true
+    },
+    {
+      title: 'Apartment with awesome views',
+      price: 400,
+      type: 'Entire Place',
+      pool: false,
+      garage: false
+    },
+    {
+      title: 'Apartment in la Rambla',
+      price: 150,
+      type: 'Private Room',
+      pool: false,
+      garage: true
+    },
+    {
+      title: 'Comfortable place in Barcelona´s center',
+      price: 390,
+      type: 'Entire place',
+      pool: true,
+      garage: true
+    },
+    {
+      title: 'Room near Sagrada Familia',
+      price: 170,
+      type: 'Private Room',
+      pool: false,
+      garage: false
+    },
+    {
+      title: 'Great house next to Camp Nou',
+      price: 140,
+      type: 'Entire place',
+      pool: true,
+      garage: true
+    },
+    {
+      title: 'New apartment with 2 beds',
+      price: 2000,
+      type: 'Entire place',
+      pool: false,
+      garage: true
+    },
+    {
+      title: 'Awesome Suite',
+      price: 230,
+      type: 'Private Room',
+      pool: false,
+      garage: false
+    },
+    {
+      title: "Apartment 10' from la Rambla",
+      price: 930,
+      type: 'Entire place',
+      pool: true,
+      garage: true
+    }
+  ];
+  
+  function roomWithPool(rooms){
+    return rooms.filter(function(rooms){
+      return rooms.pool
+    })
+  }
+
+  
+function filterNumber(nums){
+    return nums.filter(function(nums){
+      return (nums % 2!=0) && (nums>42)
+    })
+  }
+  
+  console.log(roomWithPool(places))
+console.log (filterNumber(numbers))
